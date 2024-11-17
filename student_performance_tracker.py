da51e532a59b79677abee990a1dab5681c810d1f
usman ashraf <misterusmanashraf@gmail.com>
2024-11-17 13:23:51 +0500

    comments added

---
+++ student_performance_tracker.py
@@ -0,0 +1,57 @@
+class Student():
+    def __init__(self, name: str, score: list):
+        self.name = name
+        self.score = score
+    def calculate_avg(self):
+        sum =0 
+        for mark in self.score:
+            sum =sum + mark
+        avg_marks= sum/len(self.score)
+        return avg_marks
+    def is_passing(self):
+        avg_marks=self.calculate_avg()
+        if avg_marks >= 40:
+            print("passed")
+        else:
+            print("needs improvement")
+class PerformanceTracker (Student):
+    def __init__ (self, students: dict):     #dict must contain name of student as key and objects as value.
+        super().__init__(name, score)
+        self.students = students 
+
+    def add_student(self):
+        name = input ("Enter the name of student: ")
+        subjects = int(input ("Enter number of subjects to add their marks: "))
+        score = []
+        for subject in range (1, subjects+1):
+            marks = int(input (f"Enter the marks of subject {subject}: "))
+            score.append (marks)
+    def calculate_class_avg(self):
+        avg=0
+        for value in students.values ():
+            s_avg=value.calculate_avg()
+            # obj1=students.values ()
+            # obj1.calculate_avg()
+            avg = avg+ s_avg
+            # avg+= avg
+        class_avg= avg/len(students.keys())
+        return class_avg
+    def display_student_performance (self):
+        for value in students.values ():
+            print(f"The average score of {self.name} is {value.calculate_avg()}")
+            f"The student {self.name}'s status is {value.is_passing()}"
+students: dict ={} #this dictionary will be added as attribute to the performance tracker class.
+while True: #loop to take input from user to add student name and list of numbers. these 2 attributes will be added to make objects of subject class.
+    name= input ("enter name of student: ")
+    subjects = int(input ("Enter number of subjects to add their marks: "))
+    score = []
+    for subject in range (1, subjects+1):
+        marks = int(input (f"Enter the marks of subject {subject}: "))
+        score.append (marks)
+    obj= Student(name, score) #object created every time loop run.
+    students[name]= obj # student name and object of student class is added to the the "students" dict as key and value respectively. 
+    choice = input ("Do you want to add another student (yes/no): ")
+    if choice =="no":
+        break
+p1= PerformanceTracker(students)
+p1.display_student_performance()
